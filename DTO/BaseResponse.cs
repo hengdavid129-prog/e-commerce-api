f3672228d17c8f7b042cf23345ddcfa90ffb9105
hengdavid <hengdavid129@gmail.com>
2026-06-30 18:38:22 +0700

    setup project structure to follow clean architechture pattern

---
+++ DTO/BaseResponse.cs
@@ -0,0 +1,26 @@
+﻿namespace E_Commerce_api.DTO
+{
+    public class BaseResponse<T>
+    {
+        public string? devErrorMessage { get; set; }
+        public T? data { get; }
+        public bool isSuccess { get; set; }
+        
+        private BaseResponse(bool isSuccess, string? devErrorMessage, T? data)
+        {
+            this.isSuccess = isSuccess;
+            this.devErrorMessage = devErrorMessage;
+            this.data = data;
+        }
+
+        public static BaseResponse<T> Success(T data)
+        {
+            return new BaseResponse<T>(true, null, data);
+        }
+
+        public static BaseResponse<T> Success(string devErrorMessage)
+        {
+            return new BaseResponse<T>(false, devErrorMessage, default);
+        }
+    }
+}
